dbaa46785d19667407080cac1d93d8a58dfc51a2
arielm7 <arielmv07@gmail.com>
2018-10-15 19:58:23 -0600

    problemas con la ventana soluionados

--- svm-digit-classification.py
+++ svm-digit-classification.py
@@ -5,39 +5,16 @@ Train a SVM to categorize 28x28 pixel images into digits (MNIST dataset).
 """
 
 import numpy as np
-from PIL import Image
-
-def main():
+from PIL import ImageTk, Image, ImageDraw
+import PIL
+from Tkinter import *
     
-    data = get_data()#retriving image data from MNIST
-
-    # Choose classifier configuration
-    from sklearn.svm import SVC
-    clfRBF = SVC(probability=False,  # cache_size=200,
-              kernel="rbf", C=2.8, gamma=.0073)
     
-    clfLin = SVC(probability=False,  # cache_size=200,
-              kernel="linear", C=2.8, gamma=.0073)
-
-    print("Entrenando, espere un momento...")
-
-    #examples = len(data['train']['X']) # number of training images to use
-    examples = 1100;
-    clfRBF.fit(data['train']['X'][:examples], data['train']['y'][:examples]) #training
-    #clfLin.fit(data['train']['X'][:examples], data['train']['y'][:examples]) #training
-
-    ###problem 1.3###########
-    analyze(clfRBF, data)
-    #########################
-
-    get_image(clfRBF)
 
 def get_image(clf):
    
-    
-    import draw
-    img = Image.open('digit.png').convert("L")
-    im2 = img.resize((28, 28), Image.NEAREST)
+    img = PIL.Image.open('digit.png').convert("L")
+    im2 = img.resize((28, 28), PIL.Image.NEAREST)
     a = np.array(im2)
     imageArray = a.ravel()
     imageArray = imageArray/255.0*2 - 1
@@ -147,6 +124,73 @@ def get_data():
                          'y': y_test}}
     return data
 
+def save():
+    filename = "digit.png"
+    image1.save(filename)
+
+def paint(event):
+    # python_green = "#476042"
+    event.widget
+    x1, y1 = (event.x - 1), (event.y - 1)
+    x2, y2 = (event.x + 1), (event.y + 1)
+    cv.create_oval(x1, y1, x2, y2, fill="white",width=15)
+    draw.line([x1, y1, x2, y2],fill="white",width=15)
+
 
 if __name__ == '__main__':
-    main()
+    width = 200
+    height = 200
+    center = height//2
+    white = (255, 255, 255)
+
+    data = get_data()#retriving image data from MNIST
+
+    # Choose classifier configuration
+    from sklearn.svm import SVC
+    clfRBF = SVC(probability=False,  # cache_size=200,
+              kernel="rbf", C=2.8, gamma=.0073)
+    
+    clfLin = SVC(probability=False,  # cache_size=200,
+              kernel="linear", C=2.8, gamma=.0073)
+
+    print("Entrenando, espere un momento...")
+
+    #examples = len(data['train']['X']) # number of training images to use
+    examples = 4000;
+    clfRBF.fit(data['train']['X'][:examples], data['train']['y'][:examples]) #training
+    #clfLin.fit(data['train']['X'][:examples], data['train']['y'][:examples]) #training
+
+    ###problem 1.3###########
+    analyze(clfRBF, data)
+    #########################
+    
+
+    while(True):
+        print("\n\n dibuje un digito presione listo! y luego cierre la ventana\n\n ")
+        root = Tk()
+
+        # Tkinter create a canvas to draw on
+        cv = Canvas(root, width=width, height=height, bg='white')
+        cv.pack()
+
+        # PIL create an empty image and draw object to draw on
+        # memory only, not visible
+        image1 = PIL.Image.new("RGB", (width, height), (0,0,0))
+        draw = PIL.ImageDraw.Draw(image1)
+        # do the Tkinter canvas drawings (visible)
+        
+        cv.pack(expand=YES, fill=BOTH)
+        cv.bind("<B1-Motion>", paint)
+
+        # do the PIL image/draw (in memory) drawings
+        # draw.line([0, center, width, center], green)
+
+        # PIL image can be saved as .png .jpg .gif or .bmp file (among others)
+        
+        button=Button(text="Listo!",command=save)
+        button.pack()
+        root.mainloop()
+        get_image(clfRBF)
+        seguir=input("\n\n Desea continuar? 1. SI 2. NO \n\n ")
+        if(seguir==2):
+            break
